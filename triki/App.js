99542e70d672449458c85ef041d619c4a7ca7615
betanyeli <betangelii@gmail.com>
2019-11-19 17:42:42 -0300

    adding board styles adn learning native events

--- triki/App.js
+++ triki/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  // testing react hooks with native
+  // si vas a usar hooks, recuerda importar useState && useEffects según corresponda
+  // const [initState, prevState] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text>TRIKI</Text>
-      <Text>Testing react native </Text>
+      
+
+      <View style={styles.board}>
+
+        <View style={styles.line}>
+        <Button onPress={() => alert("Peek a boo")} title="Peek"></Button>
+        </View>
+
+        <View style={styles.line}>
+        <Button onPress={() => alert("Peek a boo")} title="A"></Button>
+        </View>
+
+        <View style={styles.line}>
+        <Button onPress={() => alert("Peek a boo")} title="Boo"></Button>
+        </View>
+
+
+      </View>
+      
+
     </View>
   );
 }
@@ -13,8 +36,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center", 
+    
   },
+  board: {
+    
+    borderWidth: 1,
+    width: 300,
+    height: 300,
+    flexDirection: 'row'
+  },
+  line: {
+    flex: 1,
+    backgroundColor: "pink",
+
+    borderWidth: 1, 
+    
+  }
 });
---
+++ triki/components/square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const Square = () => {
+    return (
+        <View>
+            <Text> I'm a square</Text>
+        </View>
+     )
+}
+
+export default square;
