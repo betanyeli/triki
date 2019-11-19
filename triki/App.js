eed896228758dbe622ffbcb6686cb2896cb202e9
betanyeli <betangelii@gmail.com>
2019-11-18 23:10:21 -0300

    creating boilerplate

--- triki/App.js
+++ triki/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Holis MUNDIS</Text>
+      <Text>TRIKI</Text>
     </View>
   );
 }
